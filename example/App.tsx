b6ed479f2fa35427a35641a2b4dfe11080fc4fd6
Kimura Yusuke <u331951c@ecs.osaka-u.ac.jp>
2025-08-19 12:48:23 +0900

    refactor(app): remove unused TextInput and update placeholder text
    Simplify the App component by removing the unused TextInput and
    updating the placeholder in NitroTextInput for better clarity.

--- example/App.tsx
+++ example/App.tsx
@@ -1,11 +1,10 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, TextInput, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import { NitroTextInput } from 'react-native-nitro-text-input'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <TextInput autoCapitalize="sentences" placeholder="こんばんは！" />
       <NitroTextInput
         autoCapitalize="words"
         autoCorrect
