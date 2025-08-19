f332fe292275779dc9d45dd0dc3f71d524a88f49
Kimura Yusuke <u331951c@ecs.osaka-u.ac.jp>
2025-08-20 05:31:47 +0900

    style(example): format code for consistency and readability
    Update formatting in various example files for improved clarity and maintainability.

--- example/App.tsx
+++ example/App.tsx
@@ -1,41 +1,42 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, TextInput, View } from "react-native";
-import { NitroTextInput } from "react-native-nitro-text-input";
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, TextInput, View } from 'react-native'
+import { NitroTextInput } from 'react-native-nitro-text-input'
 
 export default function App() {
-	return (
-		<View style={styles.container}>
-			<NitroTextInput
-				allowFontScaling
-				autoCapitalize="words"
-				autoCorrect
-				autoFocus={true}
-				caretHidden={false}
-				clearButtonMode="while-editing"
-				clearTextOnFocus={false}
-				contextMenuHidden={false}
-				defaultValue=""
-				editable
-				enablesReturnKeyAutomatically
-				placeholder="Nitro Text Input🔥"
-			/>
-			<TextInput
-				clearButtonMode="always"
-				placeholder="React Native Text Input"
-				enablesReturnKeyAutomatically
-				style={{ width: "100%" }}
-			/>
-			<StatusBar style="auto" />
-		</View>
-	);
+  return (
+    <View style={styles.container}>
+      <NitroTextInput
+        allowFontScaling
+        autoCapitalize="words"
+        autoCorrect
+        autoFocus={true}
+        caretHidden={false}
+        clearButtonMode="while-editing"
+        clearTextOnFocus={false}
+        contextMenuHidden={false}
+        defaultValue=""
+        editable
+        enablesReturnKeyAutomatically
+        enterKeyHint="done"
+        placeholder="Nitro Text Input🔥"
+      />
+      <TextInput
+        clearButtonMode="always"
+        placeholder="React Native Text Input"
+        enablesReturnKeyAutomatically
+        style={{ width: '100%' }}
+      />
+      <StatusBar style="auto" />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-		gap: 32,
-	},
-});
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 32,
+  },
+})
